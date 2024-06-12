24b1f7b4105ab4a2471db21f3676d8ffa28d6e71
JJ Style <style.jj@pm.me>
2024-06-12 20:04:32 +0100

    migrate alacritty config from yaml to toml

--- home-manager/packages/alacritty/alacritty.nix
+++ home-manager/packages/alacritty/alacritty.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ alacritty alacritty-theme ];
 
-  xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
+  xdg.configFile."alacritty/alacritty.toml".source = ./alacritty.toml;
 }
