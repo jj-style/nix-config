7298ab68e71192c9bcbe954f9644b2759b9d35fc
JJ Style <style.jj@pm.me>
2024-11-24 17:52:25 +0000

    Revert "make tailscale a service"
    This reverts commit d2595384319dacf05cb0231e7e84109385151ef0.

--- system/common/tailscale/tailscale.nix
+++ system/common/tailscale/tailscale.nix
@@ -1,67 +1,49 @@
-{ lib, config, pkgs, ... }:
-with lib;
-let
-  cfg = config.services.tailscale;
-in {
+{ config, pkgs, ... }: {
 
-  options.services.tailscale = {
-    enable = mkEnableOption "tailscale service";
-    sopsTsAuthKey = mkOption {
-      type = types.str;
-      default = "tailscale_authkey";
-    };
-    extraArgs = mkOption {
-      type = types.str;
-      default = "";
-    };
+  # install tailscale
+  environment.systemPackages = with pkgs; [ tailscale jq ];
+
+  # enable tailscale service
+  services.tailscale.enable = true;
+
+  # always allow traffic from your Tailscale network
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+
+  # allow the Tailscale UDP port through the firewall
+  networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+
+  # load secret authkey from sops
+  sops.secrets."tailscale_authkey" = {
+    restartUnits = [ "tailscale-autoconnect.service" ];
   };
 
-  config = mkIf cfg.enable {
-    # install tailscale
-    environment.systemPackages = with pkgs; [ tailscale jq ];
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
 
-    # enable tailscale service
-    services.tailscale.enable = true;
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
 
-    # always allow traffic from your Tailscale network
-    networking.firewall.trustedInterfaces = [ "tailscale0" ];
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
 
-    # allow the Tailscale UDP port through the firewall
-    networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
 
-    # load secret authkey from sops
-    sops.secrets."${cfg.sopsTsAuthKey}" = {
-      restartUnits = [ "tailscale-autoconnect.service" ];
-    };
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
 
-    # create a oneshot job to authenticate to Tailscale
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [ "network-pre.target" "tailscale.service" ];
-      wants = [ "network-pre.target" "tailscale.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        # check if we are already authenticated to tailscale
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
-        # otherwise authenticate with tailscale
-        ${tailscale}/bin/tailscale up -authkey $(cat ${
-          config.sops.secrets."tailscale_authkey".path
-        }) ${cfg.extraArgs}
-      '';
-    };
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey $(cat ${
+        config.sops.secrets."tailscale_authkey".path
+      }) --reset
+    '';
   };
 }
