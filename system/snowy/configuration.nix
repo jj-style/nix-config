dba3aec11b805e31ff6dc1677f42b781b6020f5f
JJ Style <style.jj@pm.me>
2024-11-24 16:49:57 +0000

    add base config for intel-nuc

---
+++ system/snowy/configuration.nix
@@ -0,0 +1,236 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{ inputs, lib, config, pkgs, locale, timeZone, ... }:
+let 
+  hostName = "snowy";
+in {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+    ../common/nix/mynix.nix
+    ../common/tailscale/tailscale.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  # ========== BOOT ========== #
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+  boot.kernelParams = [ "quiet" "splash" "ipv6.disable=1" ];
+
+  # ========== FILESYSTEM ========== #
+
+  # sops.secrets."luks" = {};
+  # environment.etc."crypttab".text = ''
+  #   backups /dev/disk/by-uuid/6744429e-ad79-4fc8-8750-d7b0bfd64a99 ${config.sops.secrets."luks".path}
+  # '';
+
+  # ========== TIME LOCALE ========== #
+
+  # Set your time zone.
+  time.timeZone = timeZone;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = locale;
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = locale;
+    LC_IDENTIFICATION = locale;
+    LC_MEASUREMENT = locale;
+    LC_MONETARY = locale;
+    LC_NAME = locale;
+    LC_NUMERIC = locale;
+    LC_PAPER = locale;
+    LC_TELEPHONE = locale;
+    LC_TIME = locale;
+  };
+
+  # ========== PACKAGES ========== #
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages =
+    (with pkgs; [ sops rsnapshot ]);
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # ========== USERS ========== #
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    mutableUsers = false;
+    users.jj = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [ ];
+      hashedPasswordFile = "${config.sops.secrets.passwd.path}";
+    };
+  };
+
+  security.sudo = {
+    enable = true;
+    extraRules = [{
+      commands = [];
+      groups = [ "wheel" ];
+    }];
+  };
+
+  # ========== SOPS ========== #
+  sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
+    validateSopsFiles = false;
+    # This will automatically import SSH keys as age keys
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    # This is using an age key that is expected to already be in the filesystem
+    age.keyFile = "/var/lib/sops-nix/key.txt";
+    # This will generate a new key if the key specified above does not exist
+    age.generateKey = true;
+    secrets = {
+      "passwd" = {
+        neededForUsers = true;
+      };
+    };
+  };
+
+  # ========== NETWORK ========== #
+  networking.hostName = hostName; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Enable networking
+  networking.networkmanager.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  # services.resolved = {
+  #   enable = true;
+  # };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedUDPPorts = [ ];
+  networking.firewall.trustedInterfaces = [ ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  networking.enableIPv6 = false;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "24.05";
+
+  # ========== SSH ========== #
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Opinionated: forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Opinionated: use keys only.
+      # Remove if you want to SSH using passwords
+      PasswordAuthentication = false;
+    };
+  };
+
+  # ========== SERVICES ========== #
+
+  services.flatpak.enable = false;
+
+  services.mynix = {
+    enable = true;
+    enableGc = true;
+    enableAutoUpgrade = true;
+  };
+
+
+  # ========== RSNAPSHOT ========== #
+  # services.rsnapshot = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     snapshot_root	/mnt/backups/docker-data
+  #     #retain	hourly	24
+  #     retain	daily	7
+  #     retain	weekly	4
+  #     retain	monthly	12
+  #     backup	root@wilson:/home/jj/docker-data	wilson/
+  #   '';
+  #   cronIntervals = {
+  #     daily = "50 21 * * *";
+  #   };
+  # };
+}
