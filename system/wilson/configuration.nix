998efee2af0c157372e965c0c69649dcfde09c89
JJ Style <style.jj@pm.me>
2026-01-14 20:10:17 +0000

    useRoutingfeatures

--- system/wilson/configuration.nix
+++ system/wilson/configuration.nix
@@ -226,6 +226,7 @@ in {
     enable = true;
     openFirewall = true;
     authKeyFile = "${config.sops.secrets."tailscale_authkey".path}";
+    useRoutingFeatures = "client";
     extraUpFlags = ["--ssh" "--advertise-exit-node --accept-dns=false"];
   };
 
